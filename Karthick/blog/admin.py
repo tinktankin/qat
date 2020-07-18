71d03e3736e95f732eb5ad242408b5ac6d4f2cf3
Karthick-rocky99 <68415937+Karthick-rocky99@users.noreply.github.com>
2020-07-18 05:48:12 -0700

    Add files via upload

---
+++ Karthick/blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Blog
+from .models import Sing
+from .models import Multp
+from .models import Ess
+from .models import Userr
+# Register your models here.
+
+admin.site.register(Blog)
+admin.site.register(Sing)
+admin.site.register(Multp)
+admin.site.register(Ess)
+admin.site.register(Userr)
