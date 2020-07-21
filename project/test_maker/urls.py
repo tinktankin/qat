39b8ee12c4383b1d8a778d5c797d8929714952f0
sankalp <sankalp123427@gmail.com>
2020-07-21 17:51:19 +0530

    removed some urls for test makers

--- project/test_maker/urls.py
+++ project/test_maker/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = "test_maker"
 
 urlpatterns = [
-	path('', views.test, name='test'),
-	path('ques/', views.ques, name='ques'),
-	path('addmcq/', views.add_mcq, name='add_mcq'),
-	path('addopen/', views.add_open, name='add_open'),
-	path('preview/',views.preview, name ='preview'),
+	# path('', views.test, name='test'),
+	# path('ques/', views.ques, name='ques'),
+	# path('addmcq/', views.add_mcq, name='add_mcq'),
+	# path('addopen/', views.add_open, name='add_open'),
+	# path('preview/',views.preview, name ='preview'),
 	path('CreateTest/',views.create_test, name='create_test'),
 	path('viewall/',views.view_all, name='view_all'),
 	path('view_test/<str:name>',views.view_test, name='view_test'),
